24c95e4261b82459695df4a74ba574eeb6c23616
Douglas Mason <demasonjr@gmail.com>
2013-09-18 22:45:12 -0400

    adds bootstrap and depedencies from nuget package

---
+++ PDiddysPizzeria/Controllers/CouponsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PDiddysPizzeria.Services;
+
+namespace PDiddysPizzeria.Controllers
+{
+    public class CouponsController : Controller
+    {
+        //
+        // GET: /Coupons/
+
+        public ActionResult Index()
+        {
+          var customerOrderService = new CustomerOrderService();
+          var customerName = customerOrderService.GetCustomerName(lastName: "Mason");
+          var model = customerOrderService.GetCustomerOrderItems(5);
+            return View();
+        }
+
+        //
+        // GET: /Coupons/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Coupons/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Coupons/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Coupons/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Coupons/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Coupons/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Coupons/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PDiddysPizzeria/Services/CustomerOrderService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PDiddysPizzeria.Services
+{
+  public class CustomerOrderService
+  {
+    public CustomerOrderService()
+    {
+      
+    }
+
+    public string GetCustomerOrderItems(int itemId, int customerId)
+    {
+      // get customer from database
+      return "Pizza";
+    }
+
+    public string GetCustomerOrderItems(int itemId)
+    {
+      return GetCustomerOrderItems().SingleOrDefault();
+    }
+
+    public List<string> GetCustomerOrderItems()
+    {
+      return new List<string>();
+    }
+
+    public string GetCustomerName(int customerId = 1, string lastName = "Diddy")
+    {
+      var customerLastName = lastName;
+      // Go to database use customerId
+      return "Paul";
+    }
+  }
+}
